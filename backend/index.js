3e40548c060f4ccc0c5248ec46d388aa750d6b75
Joana Kelly <joanak_dev@outlook.com>
2026-03-24 12:39:37 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, ERP project!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
