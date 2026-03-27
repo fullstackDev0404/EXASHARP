14a25fa5185c4b716018a6cc3f3c4c5d7c111a26
Sadik Ahsan <sadikahasan6@gmail.com>
2026-03-27 12:23:57 +0600

    update

---
+++ client/src/lib/components/section/footer.svelte
@@ -0,0 +1,16 @@
+<footer class="flex items-center justify-center h-15 text-sm text-gray-500">
+    &copy; {new Date().getFullYear()} EXASHARP. Built by 
+    <a href="https://github.com/fullstackDev0404" 
+       target="_blank"
+       rel="noopener noreferrer"
+       class="underline px-1 underline-offset-3 hover:text-gray-700 transition-colors">
+        Joana Kelly
+    </a>
+    <span class="mx-1">&</span>
+    <a href="https://github.com/sadikahasan6" 
+       target="_blank" 
+       rel="noopener noreferrer" 
+       class="underline px-1 underline-offset-3 hover:text-gray-700 transition-colors">
+        Sadik Ahsan
+    </a>
+</footer>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script>
+    import Footer from "$lib/components/section/footer.svelte";
     import Header from "$lib/components/section/header.svelte";
+    import Button from "$lib/components/ui/button/button.svelte";
 </script>
+
+<svelte:head>
+    <title>EXASHARP - The Foundation for your ERP System</title>
+</svelte:head>
 
 <Header />
 
 <div class="flex items-center justify-center flex-col py-25">
+    <div class="pb-4 flex items-center gap-2 text-sm font-medium leading-6 text-center text-primary">
+        <div class="h-2 w-2 bg-red-300 rounded-full"></div>
+        This ERP system is under development and not yet production-ready.
+    </div>
     <h1 class="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl">
- The Foundation for your ERP System
-</h1>
+        The Foundation for your ERP System
+    </h1>
+    <p class="leading-7 not-first:mt-6 text-center max-w-3xl text-lg">
+        Experimenting with next-generation ERP: flexible, scalable, and intelligent. Built for innovation, designed to adapt, and crafted to evolve with your business.
+    </p>
+    <div class="pt-6">
+        <Button size="lg">Get Started</Button>
+        <Button size="lg" variant="outline" class="ml-4">View Documentation</Button>
+    </div>
 </div>
+
+<Footer />
