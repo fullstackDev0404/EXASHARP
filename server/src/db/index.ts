8c371bc220f89a989b0eb6be42450138a913d5b7
Sadik Ahsan <sadikahasan6@gmail.com>
2026-03-27 07:06:12 +0600

    update

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,10 +1,13 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
+import { Pool } from "pg";
 import * as schema from "./schema";
 
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-await client.connect();
-export const db = drizzle(client, { schema });
+pool.on("error", (err) => {
+  console.error("Unexpected Neon DB error:", err);
+});
+
+export const db = drizzle(pool, { schema });
