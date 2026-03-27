190c411de72feb43e4600fa5b97c422221dbbe95
Sadik Ahsan <sadikahasan6@gmail.com>
2026-03-27 07:53:50 +0600

    better-auth init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,21 @@
-import { Elysia, status, t } from 'elysia'
+import  { Context, Elysia } from 'elysia'
 import { openapi } from '@elysiajs/openapi'
 import { cors } from '@elysiajs/cors'
 import { db } from './db';
+import { auth } from './lib/auth';
 
 const app = new Elysia()
   .use(openapi()) // Elysia OpenAPI plugin (optional, but highly recommended for API documentation)
   .use(cors()) // Enable CORS for all routes (you can configure this as needed)
   .decorate('db', db) // DB for handlers
-  
+  .decorate('user', null) // Add user property to context
   .get('/', () => 'Hello, Elysia!') // Basic route to test the server
+
   .listen(3000)
+
+app.all("/api/auth/*", (ctx) => {
+  return auth.handler(ctx.request);
+});
+
 
 export type App = typeof app;
